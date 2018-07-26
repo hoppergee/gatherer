8c7282375649d0893872d50772a83313ebb3a48d
吉翔 <jx4554@gmail.com>
2018-07-27 01:13:56 +0800

    when task complete, project also complete

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,11 +5,17 @@ RSpec.describe Project do
   let(:task) { Task.new }
 
   it "considers a project with no tasks to be done" do
-    expect(project.done?).to be_truthy
+    expect(project).to be_done
   end
 
   it "knows that a project with an incomplete task is not done" do
     project.tasks << task
-    expect(project.done?).to be_falsy
+    expect(project).not_to be_done
+  end
+
+  it 'marks a project done if its tasks are done' do
+    project.tasks << task
+    task.mark_completed
+    expect(project).to be_done
   end
 end
