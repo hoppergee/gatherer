278dc45f1b196474076c2892a343c68728b89d2d
吉翔 <jx4554@gmail.com>
2018-07-27 01:41:58 +0800

    Proejct can calculate size

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,19 @@ class Project
     @tasks = []
   end
 
+  def incomplete_tasks
+    tasks.reject(&:complete?)
+  end
+
   def done?
     tasks.all?(&:complete?)
+  end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    incomplete_tasks.sum(&:size)
   end
 end
