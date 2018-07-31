21077da66fee2e6622132ce47da4b860dee11533
吉翔 <jx4554@gmail.com>
2018-07-31 09:58:43 +0800

    Use factory bot

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,33 +4,51 @@ RSpec.describe Project do
 
   it_should_behave_like "sizeable"
 
-  let(:project) { Project.new }
-  let(:task) { Task.new }
+  describe "without a task" do
+    let(:project) { FactoryBot.build_stubbed(:project) }
+    it "considers a project with no tasks to be done" do
+      expect(project).to be_done
+    end
 
-  it "considers a project with no tasks to be done" do
-    expect(project).to be_done
+    it "properly estimates a blank project" do
+      expect(project.completed_velocity).to eq(0)
+      expect(project.current_rate).to eq(0)
+      expect(project.projected_days_remaining).to be_nan
+      expect(project).not_to be_on_schedule
+    end
   end
 
-  it "knows that a project with an incomplete task is not done" do
-    project.tasks << task
-    expect(project).not_to be_done
+  describe "with a task" do
+    let(:project) { FactoryBot.build_stubbed(:project, tasks: [task]) }
+    let(:task) { FactoryBot.build_stubbed(:task) }
+
+    it "knows that a project with an incomplete task is not done" do
+      expect(project).not_to be_done
+    end
+
+    it "marks a project done if its tasks are done" do
+      task.mark_completed
+      expect(project).to be_done
+    end
   end
 
-  it 'marks a project done if its tasks are done' do
-    project.tasks << task
-    task.mark_completed
-    expect(project).to be_done
-  end
+  describe "estimates" do
+    let(:project) { FactoryBot.build_stubbed(:project,
+      tasks: [newly_done, old_done, small_not_done, large_not_done]) }
+    let(:newly_done) { FactoryBot.build_stubbed(:task,
+      size: 3, completed_at: 1.day.ago) }
+    let(:old_done) { FactoryBot.build_stubbed(:task,
+      size: 2, completed_at: 6.month.ago) }
+    let(:small_not_done) { FactoryBot.build_stubbed(:task, size: 1) }
+    let(:large_not_done) { FactoryBot.build_stubbed(:task, size: 4) }
 
-  describe "estimate" do
-    let(:project) { Project.new }
-    let(:newly_done) { Task.new(size: 3, completed_at: 1.day.ago) }
-    let(:old_done) { Task.new(size: 2, completed_at: 6.month.ago) }
-    let(:small_not_done) { Task.new(size: 1) }
-    let(:large_not_done) { Task.new(size: 4) }
+    it "can calculate total size" do
+      expect(project).to be_of_size(10)
+      expect(project).not_to be_of_size(5)
+    end
 
-    before(:example) do
-      project.tasks = [newly_done, old_done, small_not_done, large_not_done]
+    it "can calculate remaining size" do
+      expect(project).to be_of_size(5).for_incomplete_tasks_only
     end
 
     it "knows its velocity" do
@@ -57,10 +75,4 @@ RSpec.describe Project do
 
   end
 
-  it "properly handles a blank project" do
-    expect(project.completed_velocity).to eq(0)
-    expect(project.current_rate).to eq(0)
-    expect(project.projected_days_remaining).to be_nan
-    expect(project).not_to be_on_schedule
-  end
 end
