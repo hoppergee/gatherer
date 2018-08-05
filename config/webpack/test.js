daa9f37a8195eeca3f497812530a7540a77fc3db
吉翔 <jx4554@gmail.com>
2018-08-06 06:45:05 +0800

    Set karma, webpack can run a sample test

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
 const environment = require('./environment')
+environment.plugins.get("Manifest").opts.writeToFileEmit = process.env.NODE_ENV !== "test"
 
-module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig()
+config.devtool = "inline-source-map"
+
+module.exports = config
+
+// module.exports = environment.toWebpackConfig()
